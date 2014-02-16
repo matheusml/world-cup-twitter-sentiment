a9c562239c1b78ddc287bdfb4f3106031cb48f41
Matheus Lima <matheusml90@gmail.com>
2014-02-16 17:39:29 -0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WorldCupTwitterSentiment::Application.config.secret_key_base = '4e29dceaeb4fca95ed559bf97aa7bccf2ae5e8570bb5187661113bf25f7d9a2720c92cef3dbb18f3c687fb0d90425e4dce2c9baa0b2bc1d097756fab150734fd'

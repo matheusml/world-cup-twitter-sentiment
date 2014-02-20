9468d17eac9d777620347b487310d5d1d4e52aef
Matheus Lima <matheusml90@gmail.com>
2014-02-19 21:54:29 -0300

    ADds group field to squads

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140220004012) do
+ActiveRecord::Schema.define(version: 20140220005237) do
 
   create_table "squads", force: true do |t|
     t.string   "name"
     t.decimal  "positive_percentage", precision: 10, scale: 0
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "group"
   end
 
 end

9aafab8f369932433a407fbbf2adaacdd113d7bd
Matheus Lima <matheusml90@gmail.com>
2014-03-09 09:41:40 -0300

    Adds tweetstream

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 1.2'
 gem "twitter-bootstrap-rails"
+gem 'tweetstream'
 
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,19 @@
+require 'tweetstream'
+
 class HomeController < ApplicationController
 	def index
+		TweetStream.configure do |config|
+		  config.consumer_key       = 'BlpfM8bCI4RVELlc5PGhAg'
+		  config.consumer_secret    = 'IJYJ0ga6CP4sNBZ7pCgCFh73aocPXCTmbIKLYVbomIQ'
+		  config.oauth_token        = '15689757-hspmJBwuytAkFlJzKNUpvCIV0skcQbDyCKvrgTLag'
+		  config.oauth_token_secret = '0lufFh9k1j5mQ2DtJ2PswvGIJrZTQfsbxkau0Gp6U0'
+		  config.auth_method        = :oauth
+		end
+		
+		TweetStream::Client.new.track('messi', language: 'pt') do |status|
+		  puts "#{status.text}"
+		end
+
 		@squads = Squad.all
 		@active_squad = params[:squad_id] ? Squad.find(params[:squad_id]) : @squads.first
 		@players = @active_squad.players

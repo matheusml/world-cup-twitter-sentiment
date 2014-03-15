5339a1c49e647f26bb0bbc9f434b9d6e89432f64
Matheus Lima <matheusml90@gmail.com>
2014-03-15 11:02:26 -0300

    Updates relations between Tweet and Squad, Players

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,6 @@
 class Player < ActiveRecord::Base
 	validates_presence_of :name
+	
 	belongs_to :squad
+	has_many :tweets, as: :tweetable
 end
--- app/models/squad.rb
+++ app/models/squad.rb
@@ -1,4 +1,6 @@
 class Squad < ActiveRecord::Base
 	validates_presence_of :name
+	
 	has_many :players
+	has_many :tweets, as: :tweetable
 end

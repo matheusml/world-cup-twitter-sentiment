2cb95155eee441933a884174060b4daf2bd0573f
Matheus Lima <matheusml90@gmail.com>
2014-03-30 17:26:21 -0300

    Adds migration to populates fake tweets

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140323133242) do
+ActiveRecord::Schema.define(version: 20140330202126) do
 
   create_table "players", force: true do |t|
     t.string   "name"

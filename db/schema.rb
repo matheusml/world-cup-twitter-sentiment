50e040d0798427d85d77f60560b4c806b301bc7f
Matheus Lima <matheusml90@gmail.com>
2014-04-13 09:20:32 -0300

    Adds fake tweets for a squad, to dev mode

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140330202126) do
+ActiveRecord::Schema.define(version: 20140413121738) do
 
   create_table "players", force: true do |t|
     t.string   "name"

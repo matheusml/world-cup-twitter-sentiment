11cfbe700effb5e64f6b11ce0ef61e3e208aa790
Matheus Lima <matheusml90@gmail.com>
2014-04-13 10:21:10 -0300

    Adds show_tweets to dynamically changes tweets to show

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,4 +9,12 @@ class HomeController < ApplicationController
       f.series(:type=> 'spline',:name=> '% Tweets positivos', :data=> [30, 95, 75, 63.3, 71])
     end
 	end
+
+	def show_tweets
+		@player = Player.find params[:player_id]
+		@chart = LazyHighCharts::HighChart.new('graph') do |f|
+      f.options[:xAxis][:categories] = ['01/07', '02/07', '03/07', '04/07', '05/07']
+      f.series(:type=> 'spline',:name=> '% Tweets positivos', :data=> [30, 95, 75, 63.3, 71])
+    end
+	end
 end

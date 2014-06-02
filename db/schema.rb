95a8e69751bb7517876b24ed145505a77159fd19
Matheus Lima <matheusml90@gmail.com>
2014-06-01 21:27:24 -0300

    Adds migration to removes some players

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140529205158) do
+ActiveRecord::Schema.define(version: 20140602002059) do
 
   create_table "players", force: true do |t|
     t.string   "name"

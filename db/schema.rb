5e6f66e6fea2a0fa5df8f7283a5f55ed29e8642c
Matheus Lima <matheusml90@gmail.com>
2014-06-03 22:15:18 -0300

    Updates schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140602002059) do
+ActiveRecord::Schema.define(version: 20140604011147) do
 
   create_table "players", force: true do |t|
     t.string   "name"

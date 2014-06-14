e4d0ed8ed36567b2b22a3e43e976599c61f8ec34
Matheus Lima <matheusml90@gmail.com>
2014-06-14 10:05:10 -0300

    Removes some squads

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140610233908) do
+ActiveRecord::Schema.define(version: 20140614125713) do
 
   create_table "players", force: true do |t|
     t.string   "name"
